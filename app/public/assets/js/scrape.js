cb89dfece3e4522eb8cb50972968ca09fa20fc0c
Laura Boetcher <g33klaura@gmail.com>
2017-12-08 20:56:11 -0500

    Initial click function for New Articles button

---
+++ app/public/assets/js/scrape.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  
+  // On-click for "New Articles" button
+  $(".scrape-new").on("click", function() {
+    // event.preventDefault();
+    console.log("New articles clicked");
+  });
+
+})

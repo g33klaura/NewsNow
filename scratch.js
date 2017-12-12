1ba14160fd6880c34bb4db5ed857865f4d536580
Laura Boetcher <g33klaura@gmail.com>
2017-12-11 23:58:58 -0500

    Updates

--- scratch.js
+++ scratch.js
@@ -8,7 +8,7 @@ Steps to complete:
 [x] Handlebars templates
 
 Routes needed 
-[] scrape new articles
+[x] scrape new articles
 [] display scraped articles
 [] save article (and delete from scraped list?)
 [] save note to article
@@ -26,12 +26,6 @@ app/public/assets/js/scrape.js ~
 app/public/assets/js/nav.js ~ script for collapsing navbar
 
 controller/api-routes.js ~ 
-controller/html-routes.js ~ *NOT ACTUALLY SURE I NEED...
+'/' ~ gets articles that've been saved to the db and renders handlebars object
 
 */
-
-// index route loads view.html    ***worked except couldn't load js scripts
-// From 15-Sequelized>Sat>15-Post-Author_joins
-// app.get("/", function(req, res) {
-//   res.sendFile(path.join(__dirname, "app/public/index.html"));
-// });

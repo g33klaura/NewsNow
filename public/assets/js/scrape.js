d91638cfb2280d28b721be61953d368923389d27
Laura Boetcher <g33klaura@gmail.com>
2017-12-11 20:30:47 -0500

    Functions need work

--- public/assets/js/scrape.js
+++ public/assets/js/scrape.js
@@ -3,46 +3,46 @@
 // Need functions to:
 // scrapeArticles
 // saveArticle  **save in new script?
-  // ^^May need "setActive" example from Activity 10/Thurs
+// ^^May need "setActive" example from Activity 10/Thurs
 // renderArticles (on the page)
 // renderHandlebars
 // noNewArticles
 // displayAllArticles
 
 
-function renderHandlebars() {
-  // $('.article-list').html('{{> (new-block) }}');
-  console.log('Template should load');  //Doesn't fire...
-  // Needs something in api-routes?????????
-}
+// function renderHandlebars() {
+//   // $('.article-list').html('{{> (new-block) }}');
+//   console.log('Template should load');  //Doesn't fire...
+//   // Needs something in api-routes?????????
+// }
 
-function displayAllArticles() {
-  $.getJSON('/', function(data) {
-    // Need to render handlebars template
-    renderHandlebars();
-  });
-}
+// function displayAllArticles() {
+//   // $.getJSON('/', function(data) {
+//   //   // Need to render handlebars template
+//   //   renderHandlebars();
+//   // });
+// }
 
 function scrapeArticles() {
   // Function needs to display number of articles scraped or message that none new(?)
-  $.getJSON('/scrape', function(data) {
+  $.getJSON('/scrape', function (data) {
     console.log(data);
 
-    // Need to render handlebars template
-    // renderHandlebars()
+    // scrape for new, then display
+    // displayAllArticles();
   });
 }
 
 
 // MAIN PROCESS
 // ==========================================
-$(document).ready(function() {
-  
+$(document).ready(function () {
+
   // On-click for "New Articles" button
-  $('.scrape-new').on('click', function() {
+  $('.scrape-new').on('click', function () {
     // event.preventDefault();
     console.log('New articles clicked');
- 
+
     // Call function to render number of articles have been scraped OR "no new articles" message***
     scrapeArticles();
 

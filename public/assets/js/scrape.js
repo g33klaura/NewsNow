7f1fd6167ced89e8a26970d440fcb47260237da5
Laura Boetcher <g33klaura@gmail.com>
2017-12-15 21:01:10 -0500

    Working on modal function

--- public/assets/js/scrape.js
+++ public/assets/js/scrape.js
@@ -23,6 +23,22 @@
 //   // });
 // }
 
+function renderModal() {
+  // Target some empty div in index.handlebars
+  // var articleModal = 
+  // Save modal html to variable
+  // Then append that variable to the empty modal div?
+
+  $('.modal').html('<a class="waves-effect waves-light btn modal-trigger" href="#modal-articles">I is a Modal</a>');
+
+  if (numberArticles != 0) {
+    console.log('Currently ' + numberArticles + ' articles in the database.');
+    
+  } else {
+    console.log('No scraped articles in database.');
+  }
+}
+
 function scrapeArticles() {
   // Function needs to display number of articles scraped or message that none new(?)
   $.getJSON('/scrape', function (data) {
@@ -34,6 +50,8 @@ function scrapeArticles() {
     // scrape for new, then display
     // displayAllArticles();
 
+    var numberArticles = data.length;
+    renderModal();
   });
 }
 
@@ -50,6 +68,7 @@ $(document).ready(function () {
     // Call function to render number of articles have been scraped OR "no new articles" message***
     scrapeArticles();
 
+    $('#modal-articles').modal('open');
     // // Display current date & time here for testing
     // var scrapeTime = new Date(Date.now()).toLocaleString();
     // console.log(scrapeTime);
